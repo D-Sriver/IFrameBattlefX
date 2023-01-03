3259636361ced4319d167c1143fa02c4584b3a3c
D-Sriver <duez.s@proton.me>
2023-01-03 10:41:45 +0100

    add files js, change class in id, integate files JS and add var for Life,ennergy and ammo

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-import { Agent } from "./iframebattlefx";
+console.log("Chargement script.js");
+var agent;
+
+function onDirChanged(event) {
+	console.log(`Dir changed ${agent.dir}`);
+	console.log(event);
+	document.querySelector("body").innerHTML = `Dir changed ${agent.dir}`;
+}
 
 function onLoaded(event) {
-	// se connecter a pytactx et s'abonner a agent event
-	var agent = new Agent(
-		"toto",
-		"demo",
-		"demo",
-		8080,
-		"mqtt.jusdeliens.com",
-		3,
-		true
-	);
+	console.log("script chargé");
+
+	// Connecting to pytactx and subscribe to agent events
+	agent = new Agent("HappyNewYear","demo","demo", "demo", 8080, "mqtt.jusdeliens.com", 3, true);
 	agent.connect();
+
+	agent.addEventListener("dirChanged", onDirChanged);
 }
 
 document.addEventListener("DOMContentLoaded", onLoaded);
