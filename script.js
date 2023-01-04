a7b47b1521cbf0529dc92b3fe49b844c042e76e2
D-Sriver <duez.s@proton.me>
2023-01-04 11:09:14 +0100

    add transform rotate

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-console.log("Chargement script.js");
-var agent;
-
-function onDirChanged(event) {
-	console.log(`Dir changed ${agent.dir}`);
-	console.log(event);
-	document.querySelector("body").innerHTML = `Dir changed ${agent.dir}`;
+//lien vers la fonction Connected
+function onAgentConnected(event) {
+  let agent = event.src;
+  console.log(`Agent connected ${agent.id}`);
 }
-
-function onLoaded(event) {
-	console.log("script chargé");
-
-	// Connecting to pytactx and subscribe to agent events
-	agent = new Agent("HappyNewYear","demo","demo", "demo", 8080, "mqtt.jusdeliens.com", 3, true);
-	agent.connect();
-
-	agent.addEventListener("dirChanged", onDirChanged);
+//lien vers la fonction DirChanged
+function onAgentUpdated(event) {
+  let agent = event.src;
+  console.log(`Agent updated ${agent.id}`);
+  agent.lookTo((agent.dir + 1) % 4);
 }
+//lien vers la fonction DirChanged
+function onAgentDirChanged(event) {
+  let agent = event.src;
+  console.log(`Agent dir changed ${agent.dir}`);
+  //!-------------------------------------------------
+  let img = document.querySelector(".bot_img");
+  console.log(img);
+  img.style.transform = "rotate(" + agent.dir * 90 + "deg)";
+}
+//fonction qui permet de se connecter à l'agent(iframebattlefx)
+function onPageLoaded(event) {
+  let agent = new Agent(
+    "sebastien_duez",
+    "demo",
+    "demo",
+    "iframebattlefx",
+    443,
+    "mqtt.jusdeliens.com",
+    2,
+    false
+  );
+  agent.connect();
 
-document.addEventListener("DOMContentLoaded", onLoaded);
-
+  //Active les agent "Connected","Updated","dirChange"
+  agent.addEventListener("connected", onAgentConnected);
+  agent.addEventListener("updated", onAgentUpdated);
+  agent.addEventListener("dirChanged", onAgentDirChanged);
+}
+//charge le dom (defer)
+document.addEventListener("DOMContentLoaded", onPageLoaded);
