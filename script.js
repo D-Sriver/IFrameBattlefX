aab57d715dc3a892f4414d0d9382c3c94e95fb31
D-Sriver <duez.s@proton.me>
2023-01-04 15:58:53 +0100

    add function for fire

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
+var agent;
+
 //*lien vers la fonction Connected
 function onAgentConnected(event) {
-  let agent = event.src;
   console.log(`Agent connected ${agent.id}`);
 }
+
 //*lien vers la fonction Updated
 function onAgentUpdated(event) {
-  let agent = event.src;
   console.log(`Agent updated ${agent.id}`);
   agent.lookTo((agent.dir + 1) % 4);
+  let life = document.querySelector(".life_bar_bg");
+  console.log(life);
+  life.style.width = "1%";
 }
+
 //*lien vers la fonction DirChanged
 function onAgentDirChanged(event) {
-  let agent = event.src;
   console.log(`Agent dir changed ${agent.dir}`);
   let img = document.querySelector("img");
   console.log(img);
   img.style.transform = "rotate(" + agent.dir * 90 + "deg)";
 }
+function onFireButtonDown(event) {
+  console.log("fire !");
+}
+function onFireButtonUp(event) {
+  console.log("Stop fire");
+}
+function onOvaLoaded(event) {
+  let firebutton = document.querySelector("#button_shot");
+  firebutton.addEventListener("mousedown", onFireButtonDown);
+  firebutton.addEventListener("mouseup", onFireButtonUp);
+}
+
 //*fonction qui charge l'agent au démarrage de la page
 function onPageLoaded(event) {
+  let image = document.querySelector(".bloc_image_bot");
   let url_string = window.location.href;
   let url = new URL(url_string);
   let readonly = url.searchParams.get("readonly");
   let verbosity = url.searchParams.get("verbosity");
 
-//*readonly défini automatiquement sur True 
-  if ((readonly = null)) readonly = true;
+  //*readonly défini automatiquement sur True
+  if (readonly == null) readonly = true;
   else readonly = readonly == "true";
   //* expression d'operations ternaire
   //!  readonly = (readonly==null) ?   true : (readonly === 'true');
 
-//*permet de définir les informations (1 a 4 (debug))
-  if ((verbosity = null)) verbosity = 1;
+  //*permet de définir les informations (1 a 4 (debug))
+  if (verbosity == null) verbosity = 1;
   else verbosity = parseInt(verbosity);
   //* expression d'operations ternaire
   //!  readonly = (verbosity==null) ?   1 :   parseInt(verbosity);
@@ -44,10 +61,10 @@ function onPageLoaded(event) {
     "demo",
     "demo",
     "iframebattlefx",
-    443,
+    8080,
     "mqtt.jusdeliens.com",
-    2,
-    false
+    verbosity,
+    readonly
   );
   agent.connect();
 
